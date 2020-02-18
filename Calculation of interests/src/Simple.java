b0f63c56b2dcbb6faf4925f2da5f443919e1e715
satwika <satwika@android3iioy7>
2020-02-18 20:51:28 +0530

    done

---
+++ Calculation of interests/src/Simple.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.io.PrintStream;
+import java.io.IOException;
+public class Simple  {
+	private static DecimalFormat df = new DecimalFormat("0.000");
+
+     public static void main(String[] args)
+     {
+    	Scanner sc=new Scanner(System.in);
+    	double principal,time,rate,simple_interest,compound_interest,a;
+    	 System.out.println("enter principal value");
+    	 principal=sc.nextDouble();
+    	 System.out.println("enter time period");
+    	 time=sc.nextDouble();
+    	 System.out.println("enter rate value");
+    	 rate=sc.nextDouble();
+    	 simple_interest=(principal*time*rate)/100;
+    	 System.out.println("simple interest value is "+simple_interest);
+    	 a=1+(rate/100);
+    	 compound_interest=(principal*Math.pow(a, time))-principal;
+    	 System.out.format("compound interest value is  "+compound_interest);
+     }
+}
